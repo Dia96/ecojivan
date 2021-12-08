58c08c8f4c9df0953afe73f9c7a2c49f90b8b9b0
Deepanjali Tripathi <deepanjalitripathi7@gmail.com>
2021-12-08 16:45:25 +0530

    registration structure

--- lib/screens/registration.dart
+++ lib/screens/registration.dart
@@ -24,168 +24,182 @@ class _RegistrationState extends State<Registration> {
       child: Scaffold(
         backgroundColor: Colors.transparent,
         body: SingleChildScrollView(
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Image.asset(''),
-                Text("Create your Account"),
-                Align(alignment: Alignment.topLeft, child: Text('Name')),
-                TextField(),
-                Align(alignment: Alignment.topLeft, child: Text('Address')),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    Container(
-                        height: 30,
-                        width: 100,
-                        decoration: BoxDecoration(color: Colors.white),
-                        child: Text("House No*")),
-                    Container(
-                        height: 30,
-                        width: 100,
-                        decoration: BoxDecoration(color: Colors.white),
-                        child: Text("Street*")),
-                    Container(
-                        height: 30,
-                        width: 100,
-                        decoration: BoxDecoration(color: Colors.white),
-                        child: Text("Pincode*"))
-                  ],
-                ),
-                Align(alignment: Alignment.topLeft, child: Text('Gender')),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    ElevatedButton.icon(
-                        onPressed: () {},
-                        icon: Icon(Icons.female),
-                        label: Text('Female')),
-                    ElevatedButton.icon(
-                        onPressed: () {},
-                        icon: Icon(Icons.male),
-                        label: Text('Male')),
-                    ElevatedButton.icon(
-                        onPressed: () {},
-                        icon: Icon(Icons.transgender),
-                        label: Text('Others')),
-                  ],
-                ),
-                Align(alignment: Alignment.topLeft, child: Text('Looking For')),
-                CheckboxListTile(
-                  controlAffinity: ListTileControlAffinity.leading,
-                  title: Text('Healthy Lifestyle'),
-                  value: _healthy,
-                  onChanged: (value) {
-                    setState(() {
-                      _healthy = !_healthy;
-                    });
-                  },
-                ),
-                CheckboxListTile(
-                  controlAffinity: ListTileControlAffinity.leading,
-                  title: Text('Diabetes Treatment'),
-                  value: _diabetes,
-                  onChanged: (value) {
-                    setState(() {
-                      _diabetes = !_diabetes;
-                    });
-                  },
-                ),
-                CheckboxListTile(
-                  controlAffinity: ListTileControlAffinity.leading,
-                  title: Text('Obesity Treatment'),
-                  value: _obesity,
-                  onChanged: (value) {
-                    setState(() {
-                      _obesity = !_obesity;
-                    });
-                  },
-                ),
-                CheckboxListTile(
-                  controlAffinity: ListTileControlAffinity.leading,
-                  title: Text('Hypertension Treatment'),
-                  value: _hyper,
-                  onChanged: (value) {
-                    setState(() {
-                      _hyper = !_hyper;
-                    });
-                  },
-                ),
-                CheckboxListTile(
-                  controlAffinity: ListTileControlAffinity.leading,
-                  title: Text('PCOS/PCOD'),
-                  value: _pcos,
-                  onChanged: (value) {
-                    setState(() {
-                      _pcos = !_pcos;
-                    });
-                  },
-                ),
-                CheckboxListTile(
-                  controlAffinity: ListTileControlAffinity.leading,
-                  title: Text('Stress'),
-                  value: _stress,
-                  onChanged: (value) {
-                    setState(() {
-                      _stress = !_stress;
-                    });
-                  },
-                ),
-                Align(
-                    alignment: Alignment.topLeft,
-                    child: Text('Health Information(Optional')),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    Container(
-                        height: 30,
-                        width: 100,
-                        decoration: BoxDecoration(color: Colors.white),
-                        child: Text("Height")),
-                    Container(
-                        height: 30,
-                        width: 100,
-                        decoration: BoxDecoration(color: Colors.white),
-                        child: Text("Weight")),
-                    Container(
-                        height: 30,
-                        width: 100,
-                        decoration: BoxDecoration(color: Colors.white),
-                        child: Text("Age")),
-                  ],
-                ),
-                SizedBox(height: 10),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    Container(
-                        height: 30,
-                        width: 100,
-                        decoration: BoxDecoration(color: Colors.white),
-                        child: Text("HbA1c")),
-                    Container(
-                        height: 30,
-                        width: 100,
-                        decoration: BoxDecoration(color: Colors.white),
-                        child: Text("Random Blood Sugar")),
-                    Container(
-                        height: 30,
-                        width: 100,
-                        decoration: BoxDecoration(color: Colors.white),
-                        child: Text("Others")),
-                  ],
-                ),
-                Center(
-                  child: ElevatedButton(onPressed: () {
-                    Navigator.of(context).pushNamedAndRemoveUntil('/home', (route) => true);
-                  }, 
-                  child: Text("Register")
+          child: Padding(
+            padding: const EdgeInsets.all(15.0),
+            // child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Image.asset(''),
+                  Text("Create your Account"),
+                  Align(alignment: Alignment.topLeft, child: Text('Name')),
+                  TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                    ),
                   ),
-                )
-              ],
-            ),
+                  Align(alignment: Alignment.topLeft, child: Text('Address')),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Container(
+                          height: 30,
+                          width: 100,
+                          decoration: BoxDecoration(color: Colors.white),
+                          child: Text("House No*")),
+                      Container(
+                          height: 30,
+                          width: 100,
+                          decoration: BoxDecoration(color: Colors.white),
+                          child: Text("Street*")),
+                      Container(
+                          height: 30,
+                          width: 100,
+                          decoration: BoxDecoration(color: Colors.white),
+                          child: Text("Pincode*"))
+                    ],
+                  ),
+                  Align(alignment: Alignment.topLeft, child: Text('Gender')),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      ElevatedButton.icon(
+                          onPressed: () {},
+                          icon: Icon(Icons.female),
+                          label: Text('Female')),
+                      ElevatedButton.icon(
+                          onPressed: () {},
+                          icon: Icon(Icons.male),
+                          label: Text('Male')),
+                      ElevatedButton.icon(
+                          onPressed: () {},
+                          icon: Icon(Icons.transgender),
+                          label: Text('Others')),
+                    ],
+                  ),
+                  Align(
+                      alignment: Alignment.topLeft, child: Text('Looking For')),
+                  CheckboxListTile(
+                    controlAffinity: ListTileControlAffinity.leading,
+                    title: Text('Healthy Lifestyle'),
+                    value: _healthy,
+                    onChanged: (value) {
+                      setState(() {
+                        _healthy = !_healthy;
+                      });
+                    },
+                  ),
+                  CheckboxListTile(
+                    controlAffinity: ListTileControlAffinity.leading,
+                    title: Text('Diabetes Treatment'),
+                    value: _diabetes,
+                    onChanged: (value) {
+                      setState(() {
+                        _diabetes = !_diabetes;
+                      });
+                    },
+                  ),
+                  CheckboxListTile(
+                    controlAffinity: ListTileControlAffinity.leading,
+                    title: Text('Obesity Treatment'),
+                    value: _obesity,
+                    onChanged: (value) {
+                      setState(() {
+                        _obesity = !_obesity;
+                      });
+                    },
+                  ),
+                  CheckboxListTile(
+                    controlAffinity: ListTileControlAffinity.leading,
+                    title: Text('Hypertension Treatment'),
+                    value: _hyper,
+                    onChanged: (value) {
+                      setState(() {
+                        _hyper = !_hyper;
+                      });
+                    },
+                  ),
+                  CheckboxListTile(
+                    controlAffinity: ListTileControlAffinity.leading,
+                    title: Text('PCOS/PCOD'),
+                    value: _pcos,
+                    onChanged: (value) {
+                      setState(() {
+                        _pcos = !_pcos;
+                      });
+                    },
+                  ),
+                  CheckboxListTile(
+                    controlAffinity: ListTileControlAffinity.leading,
+                    title: Text('Stress'),
+                    value: _stress,
+                    onChanged: (value) {
+                      setState(() {
+                        _stress = !_stress;
+                      });
+                    },
+                  ),
+                  Align(
+                      alignment: Alignment.topLeft,
+                      child: Text('Health Information(Optional')),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Container(
+                          height: 30,
+                          width: 100,
+                          decoration: BoxDecoration(color: Colors.white),
+                          child: Text("Height")),
+                      Container(
+                          height: 30,
+                          width: 100,
+                          decoration: BoxDecoration(color: Colors.white),
+                          child: Text("Weight")),
+                      Container(
+                          height: 30,
+                          width: 100,
+                          decoration: BoxDecoration(color: Colors.white),
+                          child: Text("Age")),
+                    ],
+                  ),
+                  SizedBox(height: 10),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Container(
+                          height: 30,
+                          width: 100,
+                          decoration: BoxDecoration(color: Colors.white),
+                          child: Text("HbA1c")),
+                      Container(
+                          height: 30,
+                          width: 100,
+                          decoration: BoxDecoration(color: Colors.white),
+                          child: Text("Random Blood Sugar")),
+                      Container(
+                          height: 30,
+                          width: 100,
+                          decoration: BoxDecoration(color: Colors.white),
+                          child: Text("Others")),
+                    ],
+                  ),
+                  Center(
+                    child: ElevatedButton(
+                        onPressed: () {
+                          Navigator.of(context).pushNamedAndRemoveUntil(
+                              '/home', (route) => true);
+                        },
+                        child: Text("Register")),
+                  ),
+                  Center(
+                    child: GestureDetector(
+                      onTap: () {},
+                      child: Text("Already have account? Login")),
+                  )
+                ],
+              ),
+            // ),
           ),
         ),
       ),
