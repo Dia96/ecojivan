28e92d1b54e455b204409646b3a820ce3080e8df
Deepanjali Tripathi <deepanjalitripathi7@gmail.com>
2021-12-09 19:30:48 +0530

    profile update

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -2,41 +2,80 @@ import 'package:ecojivan/constraint.dart';
 import 'package:flutter/material.dart';
 
 class Profile extends StatelessWidget {
-  const Profile({ Key? key }) : super(key: key);
+  const Profile({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Padding(
-        padding: const EdgeInsets.only(top:20),
+        padding: const EdgeInsets.only(top: 20),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             ClipRRect(
-                    borderRadius: BorderRadius.circular(40.0), //or 15.0
-                    child: Container(
-                        height: 80.0,
-                        width: 80.0,
-                        color: Colors.brown,
-                        child: Image.asset('assets/logo.png',
-                        fit: BoxFit.cover,
-                        )),
-                  ),
-            // CircleAvatar(             
-            //   backgroundImage: AssetImage('assets/logo.png'),
-            // ),
-            Text(name,
-            style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold
-                ),
+              borderRadius: BorderRadius.circular(40.0), //or 15.0
+              child: Container(
+                  height: 80.0,
+                  width: 80.0,
+                  color: Colors.brown,
+                  child: Image.asset(
+                    'assets/logo.png',
+                    fit: BoxFit.cover,
+                  )),
+            ),
+            Text(
+              name,
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
             ),
             Card(
-              child: ListView(
+              color: Colors.transparent,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
                 children: [
-                  ListTile(
-                    leading: Icon(Icons.account_box),
-                    title: Text(edit),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 10),
+                    child: Card(
+                      child: ListTile(
+                        leading: Icon(Icons.account_box),
+                        title: Text(edit),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 10),
+                    child: Card(
+                      child: ListTile(
+                        leading: Icon(Icons.account_box),
+                        title: Text(edit),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 10),
+                    child: Card(
+                      child: ListTile(
+                        leading: Icon(Icons.account_box),
+                        title: Text(edit),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 10),
+                    child: Card(
+                      child: ListTile(
+                        leading: Icon(Icons.account_box),
+                        title: Text(edit),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 10),
+                    child: Card(
+                      child: ListTile(
+                        leading: Icon(Icons.account_box),
+                        title: Text(edit),
+                      ),
+                    ),
                   ),
                 ],
               ),
