212090d789b763ed3f73a09b9c2ef21bdfa97aa1
Deepanjali Tripathi <deepanjalitripathi7@gmail.com>
2021-12-09 18:53:56 +0530

    constraint

---
+++ lib/constraint.dart
@@ -0,0 +1,13 @@
+var hospital = 'Name of Hospital';
+var location = 'Location';
+var docname = 'Dr. Name of Doctor';
+var consult = 'CONSULT DOCTOR';
+var more = 'KNOW MORE';
+var price = '₹ 999';
+var exp = '7 years exp';
+var designation = 'Physician - MBBS/MD(Gen Med)';
+var name = 'Name';
+var acne = 'Acne';
+var runny = 'Runny';
+var create = 'Create your Account';
+var edit = 'Edit profile';
