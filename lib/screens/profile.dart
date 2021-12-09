5623bd0b6214d20a8d3b5bbe655e08f717e53f72
Deepanjali Tripathi <deepanjalitripathi7@gmail.com>
2021-12-09 18:56:22 +0530

    profile

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -1,3 +1,4 @@
+import 'package:ecojivan/constraint.dart';
 import 'package:flutter/material.dart';
 
 class Profile extends StatelessWidget {
@@ -5,10 +6,44 @@ class Profile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('Profile'),
-      
-      
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.only(top:20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            ClipRRect(
+                    borderRadius: BorderRadius.circular(40.0), //or 15.0
+                    child: Container(
+                        height: 80.0,
+                        width: 80.0,
+                        color: Colors.brown,
+                        child: Image.asset('assets/logo.png',
+                        fit: BoxFit.cover,
+                        )),
+                  ),
+            // CircleAvatar(             
+            //   backgroundImage: AssetImage('assets/logo.png'),
+            // ),
+            Text(name,
+            style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold
+                ),
+            ),
+            Card(
+              child: ListView(
+                children: [
+                  ListTile(
+                    leading: Icon(Icons.account_box),
+                    title: Text(edit),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
