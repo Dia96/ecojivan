3a2fc4383a38dd47697a3a6fe87cb460499cdd97
Deepanjali Tripathi <deepanjalitripathi7@gmail.com>
2021-12-10 19:04:34 +0530

    variable declaration

--- lib/constraint.dart
+++ lib/constraint.dart
@@ -1,5 +1,70 @@
+import 'package:flutter/cupertino.dart';
+
+const PrimaryColor = const Color(0xFF795548);
+const PrimaryColorLight = const Color(0xFFD7CCC8);
+const SecondaryColor = const Color(0xFF9E9E9E);
+const TertiaryColor = const Color(0xFFFFFFFF);
+const AppColor = const Color(0x73000000);
+
+const decoration = BoxDecoration(
+  gradient: LinearGradient(
+  begin: Alignment.centerLeft,
+  end: Alignment.centerRight,
+  colors: [SecondaryColor, PrimaryColor],
+));
+
+const bold = TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: AppColor, decoration: TextDecoration.none);
+const namestyle = TextStyle(fontSize: 15, fontWeight: FontWeight.bold);
+const bottomstyle = TextStyle(color: TertiaryColor);
+
+const paddingall = EdgeInsets.all(15.0);
+const paddingtb = EdgeInsets.only(top: 10, bottom: 15);
+const top = EdgeInsets.only(top: 10);
+const lefttop = EdgeInsets.only(left: 15, top: 15);
+
+                        
+
+var login = 'Login';
+var add = 'Address';
+var add1 = 'House No* ';
+var add2 = 'Street*';
+var add3 = 'Pincode*';
+var gender = 'Gender';
+var male = 'Male';
+var female = 'Female';
+var trans = 'Others';
+var next = 'Next';
+var health = 'Healthy Lifestyle';
+var diab = 'Diabetes Treatment';
+var obese = 'Obesity Treatment';
+var hyper = 'Hypertension Treatment';
+var pcos = 'PCOS/PCOD';
+var stress = 'Stress';
+var enter = 'Enter your registered email id or phone number below';
+var enterotp = 'Enter OTP sent on your registered Email/Phone Number';
+var looking = 'Looking For';
+var infohealth = 'Health Information (Optional)';
+var hb = 'HbA1c';
+var sugar = 'Random Blood Sugar';
 var hospital = 'Name of Hospital';
 var location = 'Location';
+var settings = 'Settings';
+var reg = 'Register';
+var selectweight = 'Select Weight';
+var selectheight = 'Select Height';
+var height = 'Height';
+var weight = 'weight';
+var age = 'Age';
+var existinguser = 'Already have account? Login';
+var aboutus = 'About us';
+var username = 'User name';
+var useremail = 'user@gmail.com';
+var email = 'Email';
+var pass = 'Password';
+var phone = 'Email or Number';
+var signin = 'Sign In';
+var otp = 'Send OTP';
+var about = 'About';
 var docname = 'Dr. Name of Doctor';
 var consult = 'CONSULT DOCTOR';
 var more = 'KNOW MORE';
@@ -11,3 +76,29 @@ var acne = 'Acne';
 var runny = 'Runny';
 var create = 'Create your Account';
 var edit = 'Edit profile';
+var bottom1 = 'Explore';
+var bottom2 = 'Upgrade';
+var bottom3 = 'Consult';
+var bottom4 = 'Profile';
+var diet1 = 'Breakfast';
+var diet2 = 'Morning Snacks';
+var diet3 = 'Lunch';
+var diet4 = 'Evening Snacks';
+var diet5 = 'Dinner';
+var cal = '730 cal';
+var quantity = 'Quantity';
+var foodname = 'Dish Name';
+var nutrition = 'Nutritional Chart';
+var recipie = 'Recipies';
+var proteins = 'Proteins';
+var fats = 'Fats';
+var carbs = 'Carbs';
+var fibre = 'Fibre';
+var nutriinfo = 'Nutritional information (Per 100 gm)';
+var ingredients = 'Ingredients';
+var steps = 'Preparation Steps';
+var cookingsteps = 'Preparation Steps in detail';
+var duration = '10 Mins';
+
+
+
