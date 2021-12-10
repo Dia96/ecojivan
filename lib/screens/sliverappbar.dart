e48d4435d8937cf6d8627bd9146f5181986e8bfb
Deepanjali Tripathi <deepanjalitripathi7@gmail.com>
2021-12-10 18:52:58 +0530

    collapsing appbar with tabbar

---
+++ lib/screens/sliverappbar.dart
@@ -0,0 +1,81 @@
+import 'package:ecojivan/constraint.dart';
+import 'package:ecojivan/screens/nutrition.dart';
+import 'package:ecojivan/screens/recipies.dart';
+import 'package:flutter/material.dart';
+
+class MainCollapsingToolbar extends StatefulWidget {
+  @override
+  _MainCollapsingToolbarState createState() => _MainCollapsingToolbarState();
+}
+
+class _MainCollapsingToolbarState extends State<MainCollapsingToolbar> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: DefaultTabController(
+        length: 2,
+        child: NestedScrollView(
+          headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+            return <Widget>[
+              SliverAppBar(
+                expandedHeight: 200.0,
+                floating: false,
+                pinned: true,
+                flexibleSpace: FlexibleSpaceBar(
+                    centerTitle: true,
+                  
+                    background: Image.asset('assets/logo.png',
+                    fit: BoxFit.cover,
+                    ),
+                    ),
+              ),
+              SliverPersistentHeader(
+                delegate: _SliverAppBarDelegate(
+                  TabBar(
+                    labelColor: Colors.black87,
+                    unselectedLabelColor: Colors.grey,
+                    tabs: [
+                      Tab(icon: Icon(Icons.info), text: nutrition),
+                      Tab(icon: Icon(Icons.food_bank_rounded), text: recipie),
+                    ],
+                  ),
+                ),
+                pinned: true,
+              ),
+            ];
+          },
+          body: TabBarView(
+          children: [
+            NutrionalChart(),
+            Recipe(),      
+          ],
+        ),
+        ),
+      ),
+    );
+  }
+}
+
+class _SliverAppBarDelegate extends SliverPersistentHeaderDelegate {
+  _SliverAppBarDelegate(this._tabBar);
+
+  final TabBar _tabBar;
+
+  @override
+  double get minExtent => _tabBar.preferredSize.height;
+  @override
+  double get maxExtent => _tabBar.preferredSize.height;
+
+  @override
+  Widget build(
+      BuildContext context, double shrinkOffset, bool overlapsContent) {
+    return new Container(
+      child: _tabBar,
+    );
+  }
+
+  @override
+  bool shouldRebuild(_SliverAppBarDelegate oldDelegate) {
+    return false;
+  }
+}
