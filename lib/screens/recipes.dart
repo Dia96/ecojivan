f7a08c7b260ddda039ebc03dd4560d632ec367cf
Deepanjali Tripathi <deepanjalitripathi7@gmail.com>
2021-12-10 19:01:54 +0530

    recipe tabview

---
+++ lib/screens/recipes.dart
@@ -0,0 +1,91 @@
+import 'package:ecojivan/constraint.dart';
+import 'package:flutter/material.dart';
+
+class Recipe extends StatefulWidget {
+  const Recipe({Key? key}) : super(key: key);
+
+  @override
+  _RecipeState createState() => _RecipeState();
+}
+
+class _RecipeState extends State<Recipe> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Padding(
+          padding: lefttop,
+          child: Align(
+            alignment: Alignment.bottomLeft,
+            child: Text(
+              foodname,
+              style: bold,
+            ),
+          ),
+        ),
+        Padding(
+          padding: lefttop,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Row(
+                children: [
+                  Icon(Icons.lock_clock),
+                  Text(duration),
+                ],
+              ),
+              Row(
+                children: [
+                  Icon(Icons.lock_clock),
+                  Text(duration),
+                ],
+              ),
+              Row(
+                children: [
+                  Icon(Icons.food_bank),
+                  Text(cal),
+                ],
+              ),
+            ],
+          ),
+        ),
+        Padding(
+          padding: lefttop,
+          child: Align(
+            alignment: Alignment.bottomLeft,
+            child: Text(
+              ingredients,
+              style: bold,
+            ),
+          ),
+        ),
+        ListTile(
+          leading: Text(ingredients),
+          trailing: Text(cal),
+        ),
+        ListTile(
+          leading: Text(ingredients),
+          trailing: Text(cal),
+        ),
+        Padding(
+          padding: lefttop,
+          child: Align(
+            alignment: Alignment.bottomLeft,
+            child: Text(
+              steps,
+              style: bold,
+            ),
+          ),
+        ),
+        ListTile(
+          leading: Icon(Icons.circle),
+          title: Text(cal),
+        ),
+        ListTile(
+          leading: Icon(Icons.circle),
+          title: Text(cal),
+        ),
+      ],
+    );
+  }
+}
