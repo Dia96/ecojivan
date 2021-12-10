fb9cc1d3dd88ffc4772b011b4b2b4c2f22520eb7
Deepanjali Tripathi <deepanjalitripathi7@gmail.com>
2021-12-10 18:52:11 +0530

    subscription

---
+++ lib/screens/subscription.dart
@@ -0,0 +1,19 @@
+import 'package:ecojivan/constraint.dart';
+import 'package:flutter/material.dart';
+
+class Subscribe extends StatefulWidget {
+  const Subscribe({ Key? key }) : super(key: key);
+
+  @override
+  _SubscribeState createState() => _SubscribeState();
+}
+
+class _SubscribeState extends State<Subscribe> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: decoration,
+      
+    );
+  }
+}
