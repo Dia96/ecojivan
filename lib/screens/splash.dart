fe9aec6d492fb29e0f68c6370d51e3825752cdb6
Deepanjali Tripathi <deepanjalitripathi7@gmail.com>
2021-12-10 18:52:30 +0530

    splashscreen

--- lib/screens/splash.dart
+++ lib/screens/splash.dart
@@ -1,25 +1,26 @@
-// import 'dart:async';
+import 'dart:async';
 
-// import 'package:flutter/material.dart';
+import 'package:ecojivan/constraint.dart';
+import 'package:flutter/material.dart';
 
-// class SplashScreen extends StatefulWidget {
-// @override
-// _SplashScreenState createState() => _SplashScreenState();
-// }
-// class _SplashScreenState extends State<SplashScreen> {
-// @override
-// void initState() {
-// 	super.initState();
-// 	Timer(Duration(seconds: 3),
-// 		()=>
-//     Navigator.of(context).pushNamedAndRemoveUntil('/register', (route) => false),
-// 		);
-// }
-// @override
-// Widget build(BuildContext context) {
-// 	return Container(
-// 	color: Colors.white,
-// 	child:FlutterLogo(size:MediaQuery.of(context).size.height)
-// 	);
-// }
-// }
+class SplashScreen extends StatefulWidget {
+@override
+_SplashScreenState createState() => _SplashScreenState();
+}
+class _SplashScreenState extends State<SplashScreen> {
+@override
+void initState() {
+	super.initState();
+	Timer(Duration(seconds: 3),
+		()=>
+    Navigator.of(context).pushNamedAndRemoveUntil('/register', (route) => false),
+		);
+}
+@override
+Widget build(BuildContext context) {
+	return Container(
+	color: TertiaryColor,
+	child:FlutterLogo(size:MediaQuery.of(context).size.height)
+	);
+}
+}
