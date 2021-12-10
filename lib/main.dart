ead887e8cd7ec1e81cd33ddd6ebade452ed25e6a
Deepanjali Tripathi <deepanjalitripathi7@gmail.com>
2021-12-10 18:56:11 +0530

    routing update

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
 import 'dart:async';
 import 'package:ecojivan/screens/bottomnav.dart';
+import 'package:ecojivan/screens/diet.dart';
 import 'package:ecojivan/screens/doctors.dart';
+import 'package:ecojivan/screens/edit.dart';
 import 'package:ecojivan/screens/login.dart';
 import 'package:ecojivan/screens/registerthree.dart';
 import 'package:ecojivan/screens/registertwo.dart';
 import 'package:ecojivan/screens/registration.dart';
+import 'package:ecojivan/screens/sliverappbar.dart';
+import 'package:ecojivan/screens/splash.dart';
 import 'package:flutter/material.dart';
 void main() {
 runApp(MyApp());
@@ -20,13 +24,16 @@ Widget build(BuildContext context) {
 	debugShowCheckedModeBanner: false,
   initialRoute: '/',
       routes: <String, WidgetBuilder>{
-        '/': (context) => Registration(),
+        '/': (context) => SplashScreen(),
+        '/register': (context) => Registration(),
         '/rtwo': (context) => RegistrationTwo(),
         '/rthree': (context) => RegistrationThree(),
         '/login': (context) => Login(),
-        '/register': (context) => Registration(),
         '/home': (context) => BottomNav(),
         '/doc': (context) => Doctors(),
+        '/edit': (context) => EditProfile(),
+        '/diet': (context) => Diet(),
+        '/sliver': (context) => MainCollapsingToolbar()
         
       },
 	);
