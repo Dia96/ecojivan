429be1841bbf9de75170449cfa3ef6487828acbf
Deepanjali Tripathi <deepanjalitripathi7@gmail.com>
2021-12-11 19:27:44 +0530

    initial update

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'package:ecojivan/extra.dart';
 import 'package:ecojivan/screens/bottomnav.dart';
 import 'package:ecojivan/screens/diet.dart';
 import 'package:ecojivan/screens/doctors.dart';
@@ -9,37 +10,36 @@ import 'package:ecojivan/screens/registertwo.dart';
 import 'package:ecojivan/screens/registration.dart';
 import 'package:ecojivan/screens/sliverappbar.dart';
 import 'package:ecojivan/screens/splash.dart';
+import 'package:ecojivan/screens/verification.dart';
 import 'package:flutter/material.dart';
+
 void main() {
-runApp(MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-@override
-Widget build(BuildContext context) {
-	return MaterialApp(
-	theme: ThemeData(
-		primarySwatch: Colors.brown,
-	),
-	debugShowCheckedModeBanner: false,
-  initialRoute: '/',
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        //primarySwatch: Colors.brown,
+        primarySwatch: Colors.green,
+      ),
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/',
       routes: <String, WidgetBuilder>{
         '/': (context) => SplashScreen(),
+        '/verification': (context) => Verification(),
         '/register': (context) => Registration(),
         '/rtwo': (context) => RegistrationTwo(),
         '/rthree': (context) => RegistrationThree(),
-        '/login': (context) => Login(),
+        //'/login': (context) => Login(),
         '/home': (context) => BottomNav(),
         '/doc': (context) => Doctors(),
         '/edit': (context) => EditProfile(),
         '/diet': (context) => Diet(),
-        '/sliver': (context) => MainCollapsingToolbar()
-        
+        '/sliver': (context) => MainCollapsingToolbar(),
       },
-	);
+    );
+  }
 }
-}
-
-
-
-
