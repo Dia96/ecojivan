b7130fa8f30b048ab64000daa34edf8ba90756e9
Deepanjali Tripathi <deepanjalitripathi7@gmail.com>
2021-12-11 19:28:11 +0530

    edit profile

--- lib/screens/edit.dart
+++ lib/screens/edit.dart
@@ -19,82 +19,82 @@ class _EditProfileState extends State<EditProfile> {
       //   colors: [SecondaryColor, PrimaryColor],
       // )),
       //return Center(
-      child: Padding(
-        padding: const EdgeInsets.only(top: 20),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            ClipRRect(
-              borderRadius: BorderRadius.circular(40.0), //or 15.0
-              child: Container(
-                  height: 80.0,
-                  width: 80.0,
-                  color: Colors.brown,
-                  child: Image.asset(
-                    'assets/logo.png',
-                    fit: BoxFit.cover,
-                  )),
-            ),
-            Text(
-              name,
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-            ),
-            Card(
-              color: Colors.transparent,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  Padding(
-                    padding: top,
-                    child: Card(
-                      child: ListTile(
-                        leading: Icon(Icons.account_box),
-                        title: Text(edit),
-                      ),
-                    ),
-                  ),
-                  Padding(
-                    padding: top,
-                    child: Card(
-                      child: ListTile(
-                        leading: Icon(Icons.account_box),
-                        title: Text(edit),
-                      ),
-                    ),
-                  ),
-                  Padding(
-                    padding: top,
-                    child: Card(
-                      child: ListTile(
-                        leading: Icon(Icons.account_box),
-                        title: Text(edit),
-                      ),
-                    ),
-                  ),
-                  Padding(
-                    padding: top,
-                    child: Card(
-                      child: ListTile(
-                        leading: Icon(Icons.account_box),
-                        title: Text(edit),
-                      ),
-                    ),
-                  ),
-                  Padding(
-                    padding: top,
-                    child: Card(
-                      child: ListTile(
-                        leading: Icon(Icons.account_box),
-                        title: Text(edit),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            )
-          ],
-        ),
-      ),
+      // child: Padding(
+      //   padding: const EdgeInsets.only(top: 20),
+      //   child: Column(
+      //     crossAxisAlignment: CrossAxisAlignment.center,
+      //     children: [
+      //       ClipRRect(
+      //         borderRadius: BorderRadius.circular(40.0), //or 15.0
+      //         child: Container(
+      //             height: 80.0,
+      //             width: 80.0,
+      //             color: Colors.brown,
+      //             child: Image.asset(
+      //               'assets/logo.png',
+      //               fit: BoxFit.cover,
+      //             )),
+      //       ),
+      //       Text(
+      //         name,
+      //         style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+      //       ),
+      //       Card(
+      //         color: Colors.transparent,
+      //         child: Column(
+      //           mainAxisAlignment: MainAxisAlignment.spaceAround,
+      //           children: [
+      //             Padding(
+      //               padding: top,
+      //               child: Card(
+      //                 child: ListTile(
+      //                   leading: Icon(Icons.account_box),
+      //                   title: Text(edit),
+      //                 ),
+      //               ),
+      //             ),
+      //             Padding(
+      //               padding: top,
+      //               child: Card(
+      //                 child: ListTile(
+      //                   leading: Icon(Icons.account_box),
+      //                   title: Text(edit),
+      //                 ),
+      //               ),
+      //             ),
+      //             Padding(
+      //               padding: top,
+      //               child: Card(
+      //                 child: ListTile(
+      //                   leading: Icon(Icons.account_box),
+      //                   title: Text(edit),
+      //                 ),
+      //               ),
+      //             ),
+      //             Padding(
+      //               padding: top,
+      //               child: Card(
+      //                 child: ListTile(
+      //                   leading: Icon(Icons.account_box),
+      //                   title: Text(edit),
+      //                 ),
+      //               ),
+      //             ),
+      //             Padding(
+      //               padding: top,
+      //               child: Card(
+      //                 child: ListTile(
+      //                   leading: Icon(Icons.account_box),
+      //                   title: Text(edit),
+      //                 ),
+      //               ),
+      //             ),
+      //           ],
+      //         ),
+      //       )
+      //     ],
+      //   ),
+      // ),
     );
     //);
   }
