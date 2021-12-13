bb3505e9bdacb52548536e2f6c1e772ebf9615da
Deepanjali Tripathi <deepanjalitripathi7@gmail.com>
2021-12-13 15:43:24 +0530

    searchbar in consult

--- lib/screens/consult.dart
+++ lib/screens/consult.dart
@@ -1,170 +1,131 @@
-import 'package:ecojivan/constraint.dart';
+import 'dart:math';
+import 'package:flappy_search_bar_ns/flappy_search_bar_ns.dart';
+import 'package:flappy_search_bar_ns/scaled_tile.dart';
 import 'package:flutter/material.dart';
+import 'package:multiselectchipgroup/multiselectchipgroup.dart';
 
-class Consult extends StatelessWidget {
-  const Consult({Key? key}) : super(key: key);
+import '../constraint.dart';
+
+class Post {
+  final String title;
+  final String body;
+
+  Post(this.title, this.body);
+}
+
+class Consult extends StatefulWidget {
+  @override
+  _ConsultState createState() => _ConsultState();
+}
+
+class _ConsultState extends State<Consult> {
+  final SearchBarController<Post> _searchBarController = SearchBarController();
+  bool isReplay = false;
+
+  Future<List<Post>> _getALlPosts(String? text) async {
+    await Future.delayed(Duration(seconds: text!.length == 4 ? 10 : 1));
+    if (isReplay) return [Post("Replaying !", "Replaying body")];
+    if (text.length == 5) throw Error();
+    if (text.length == 6) return [];
+    List<Post> posts = [];
+
+    var random = new Random();
+    for (int i = 0; i < 10; i++) {
+      posts.add(
+        Post("$text $i", "body random number : ${random.nextInt(100)}"),
+      );
+    }
+    return posts;
+  }
 
   @override
   Widget build(BuildContext context) {
-    final double itemHeight = 5;
-    final double itemWidth = 15;
-
-    return Column(
-      children: [
-        Align(
-          alignment: Alignment.bottomLeft,
-          child: Text(
-            'Select your Concern',
-            style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
-          ),
-        ),
-        Flexible(
-          child: GridView(
-            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2,
-              childAspectRatio: (itemWidth / itemHeight),
+    return Scaffold(
+      body: Column(
+        children: [
+          Align(
+            alignment: Alignment.bottomLeft,
+            child: Text(
+              'Select your Concern',
+              style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
             ),
-            children: [
-              Card(
-                child: ListTile(
-                  leading: ClipRRect(
-                    borderRadius: BorderRadius.circular(10.0), //or 15.0
-                    child: Container(
-                        height: 40.0,
-                        width: 40.0,
-                        color: PrimaryColor,
-                        child: Image.asset('assets/logo.png')),
-                  ),
-                  title: Text('Body Pain'),
-                ),
-              ),
-              Card(
-                child: ListTile(
-                  leading: ClipRRect(
-                    borderRadius: BorderRadius.circular(10.0), //or 15.0
-                    child: Container(
-                        height: 40.0,
-                        width: 40.0,
-                        color: PrimaryColor,
-                        child: Image.asset('assets/logo.png')),
-                  ),
-                  title: Text('Vomiting'),
-                ),
-              ),
-              Card(
-                child: ListTile(
-                  leading: ClipRRect(
-                    borderRadius: BorderRadius.circular(10.0), //or 15.0
-                    child: Container(
-                        height: 40.0,
-                        width: 40.0,
-                        color: PrimaryColor,
-                        child: Image.asset('assets/logo.png')),
-                  ),
-                  title: Text('a'),
-                ),
-              ),
-              Card(
-                child: ListTile(
-                  leading: ClipRRect(
-                    borderRadius: BorderRadius.circular(10.0), //or 15.0
-                    child: Container(
-                        height: 40.0,
-                        width: 40.0,
-                        color: PrimaryColor,
-                        child: Image.asset('assets/logo.png')),
-                  ),
-                  title: Text(acne),
-                ),
-              ),
-              Card(
-                child: ListTile(
-                  leading: ClipRRect(
-                    borderRadius: BorderRadius.circular(10.0), //or 15.0
-                    child: Container(
-                        height: 40.0,
-                        width: 40.0,
-                        color: PrimaryColor,
-                        child: Image.asset('assets/logo.png')),
-                  ),
-                  title: Text('Fever'),
-                ),
-              ),
-              Card(
-                child: ListTile(
-                  leading: ClipRRect(
-                    borderRadius: BorderRadius.circular(10.0), //or 15.0
-                    child: Container(
-                        height: 40.0,
-                        width: 40.0,
-                        color: PrimaryColor,
-                        child: Image.asset('assets/logo.png')),
-                  ),
-                  title: Text('Sneezing'),
-                ),
-              ),
-              Card(
-                child: ListTile(
-                  leading: ClipRRect(
-                    borderRadius: BorderRadius.circular(10.0), //or 15.0
-                    child: Container(
-                        height: 40.0,
-                        width: 40.0,
-                        color: PrimaryColor,
-                        child: Image.asset('assets/logo.png')),
-                  ),
-                  title: Text(runny),
-                ),
-              ),
-              Card(
-                child: ListTile(
-                  leading: ClipRRect(
-                    borderRadius: BorderRadius.circular(10.0), //or 15.0
-                    child: Container(
-                        height: 40.0,
-                        width: 40.0,
-                        color: PrimaryColor,
-                        child: Image.asset('assets/logo.png')),
-                  ),
-                  title: Text('Headache'),
-                ),
-              ),
-              Card(
-                child: ListTile(
-                  leading: ClipRRect(
-                    borderRadius: BorderRadius.circular(10.0), //or 15.0
-                    child: Container(
-                        height: 40.0,
-                        width: 40.0,
-                        color: PrimaryColor,
-                        child: Image.asset('assets/logo.png')),
-                  ),
-                  title: Text('Cough'),
-                ),
-              ),
-              Card(
-                child: ListTile(
-                  leading: ClipRRect(
-                    borderRadius: BorderRadius.circular(10.0), //or 15.0
-                    child: Container(
-                        height: 40.0,
-                        width: 40.0,
-                        color: PrimaryColor,
-                        child: Image.asset('assets/logo.png')),
-                  ),
-                  title: Text('Loose Motion'),
-                ),
-              ),
-            ],
           ),
+          Expanded(
+            child: SafeArea(
+              child: SearchBar<Post>(
+                searchBarPadding: EdgeInsets.symmetric(horizontal: 10),
+                headerPadding: EdgeInsets.symmetric(horizontal: 10),
+                listPadding: EdgeInsets.symmetric(horizontal: 10),
+                onSearch: _getALlPosts,
+                searchBarController: _searchBarController,
+                onError: (error) => Text('ERROR: ${error.toString()}'),
+                cancellationWidget: Text("Cancel"),
+                emptyWidget: Text("empty"),
+                indexedScaledTileBuilder: (int index) =>
+                    ScaledTile.count(1, index.isEven ? 2 : 1),
+                header: SingleChildScrollView(
+                  child: Column(
+                    children: <Widget>[
+                      Align(
+                        alignment: Alignment.bottomLeft,
+                        child: Text(
+                          'Most selected Issues',
+                          style: TextStyle(
+                              fontSize: 15, fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                      MultiSelectChipGroup(
+                        items: [fever, gas, motion, blocked, sneeze, acne, rash, period, spots, pregnancy, darkcircle, vomit, headache, constipation, runny, abdominal, hairfall, cough],
+                        onSelectionChanged: (values) {
+                          print(values);
+                        },
+                        horizontalChipSpacing: 10,
+                        selectedColor: Colors.green,
+                        disabledColor: Colors.white,
+                        leftCommonIcon: Icons.perm_identity,
+                        leftIcons: [
+                          Icons.alarm,
+                          Icons.ac_unit,
+                          Icons.accessibility,
+                          Icons.account_balance,
+                          Icons.perm_identity,
+                          Icons.perm_identity,
+                          Icons.alarm,
+                          Icons.ac_unit,
+                          Icons.accessibility,
+                          Icons.account_balance,
+                          Icons.perm_identity,
+                          Icons.feed,
+                          Icons.alarm,
+                          Icons.ac_unit,
+                          Icons.accessibility,
+                          Icons.account_balance,
+                          Icons.perm_identity,
+                          Icons.perm_identity,        
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+                onCancelled: () {
+                  print("Cancelled triggered");
+                },
+                mainAxisSpacing: 10,
+                crossAxisSpacing: 10,
+                crossAxisCount: 1,
+                onItemFound: (Post? post, int index) {
+                  return Text(post!.title);
+                },
+              ),
+            ),
+          ),
+          
+        ],
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () {}, 
+        label: Text('Choose Doctor')
         ),
-        ElevatedButton(
-            onPressed: () {
-              Navigator.of(context)
-                  .pushNamedAndRemoveUntil('/doc', (route) => true);
-            },
-            child: Text('Choose Doctor'))
-      ],
     );
   }
 }
