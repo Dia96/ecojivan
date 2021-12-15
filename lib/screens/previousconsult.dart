dad39f85c9c8877775da2546e86b4ba9e94757d6
Deepanjali Tripathi <deepanjalitripathi7@gmail.com>
2021-12-15 18:49:56 +0530

    previous consult

---
+++ lib/screens/previousconsult.dart
@@ -0,0 +1,18 @@
+import 'package:ecojivan/constraint.dart';
+import 'package:flutter/material.dart';
+
+class PreviousConsultancy extends StatefulWidget {
+  const PreviousConsultancy({ Key? key }) : super(key: key);
+
+  @override
+  _PreviousConsultancyState createState() => _PreviousConsultancyState();
+}
+
+class _PreviousConsultancyState extends State<PreviousConsultancy> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(nothing),
+    );
+  }
+}
