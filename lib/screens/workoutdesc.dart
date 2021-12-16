313d3b0e50229dcbbaa2ecf7bac8778eb8f1f439
Deepanjali Tripathi <deepanjalitripathi7@gmail.com>
2021-12-16 15:26:18 +0530

    workout description screen

---
+++ lib/screens/workoutdesc.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+import '../constraint.dart';
+
+class WorkoutDescription extends StatefulWidget {
+  const WorkoutDescription({ Key? key }) : super(key: key);
+
+  @override
+  _WorkoutDescriptionState createState() => _WorkoutDescriptionState();
+}
+
+class _WorkoutDescriptionState extends State<WorkoutDescription> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+       appBar: AppBar(
+        title: Text('Workout Plan'),
+        flexibleSpace: Container(
+          decoration: gradientdecoration,
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Image.asset('assets/workoutone.gif'),
+            Text(exname, style: namestyle,),
+            Text(dummy),
+          ],
+        ),
+      ),
+    );
+  }
+}
