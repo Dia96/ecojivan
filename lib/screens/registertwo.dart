84968caae49007013e7709cedf83f2afa858dbd1
Deepanjali Tripathi <deepanjalitripathi7@gmail.com>
2021-12-17 18:44:32 +0530

    reg view updated with email bloodgroup

--- lib/screens/registertwo.dart
+++ lib/screens/registertwo.dart
@@ -1,6 +1,10 @@
 import 'package:dropdown_search/dropdown_search.dart';
 import 'package:ecojivan/constraint.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:multiselectchipgroup/multiselectchipgroup.dart';
+import 'package:syncfusion_flutter_datepicker/datepicker.dart';
+import 'package:intl/intl.dart';
 
 class RegistrationTwo extends StatefulWidget {
   @override
@@ -8,12 +12,6 @@ class RegistrationTwo extends StatefulWidget {
 }
 
 class _RegistrationTwoState extends State<RegistrationTwo> {
-  bool _healthy = false;
-  bool _diabetes = false;
-  bool _obesity = false;
-  bool _hyper = false;
-  bool _pcos = false;
-  bool _stress = false;
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -30,80 +28,176 @@ class _RegistrationTwoState extends State<RegistrationTwo> {
                 mainAxisAlignment: MainAxisAlignment.center,
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  Align(
+
+                   Padding(
+                  padding: const EdgeInsets.only(top: 10, bottom: 5, right: 10),
+                  child: Align(
                       alignment: Alignment.topLeft,
                       child: Text(
-                        infohealth,
+                        looking,
                         style: namestyle,
                       )),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 20, bottom: 15),
-                    child: DropdownSearch<String>(
-                        mode: Mode.MENU,
-                        items: [
-                          "110cm",
-                          "120cm",
-                          "130cm",
-                          '140cm',
-                          "150cm",
-                          "160cm",
-                          "170cm",
-                          '180cm'
-                        ],
-                        label: height,
-                        onChanged: print,
-                        selectedItem: selectheight),
-                  ),
-                  Padding(
-                    padding: paddingtb,
-                    child: DropdownSearch<String>(
-                        mode: Mode.MENU,
-                        items: [
-                          "30kg",
-                          "31Kg",
-                          "32Kg",
-                          '33Kg',
-                          "34Kg",
-                          "35Kg",
-                          "36Kg",
-                          '37Kg'
-                              "38kg",
-                          "39Kg",
-                          "40Kg",
-                          '41Kg',
-                          "42Kg",
-                          "43Kg",
-                          "44Kg",
-                          '45Kg'
-                        ],
-                        label: weight,
-                        selectedItem: selectweight),
-                  ),
-                  Padding(
-                    padding: paddingtb,
-                    child: DropdownSearch<String>(
-                        mode: Mode.MENU,
-                        items: [
-                          "14",
-                          "15",
-                          "16",
-                          '17',
-                          "18",
-                          "19",
-                          "20",
-                          '21',
-                          "22",
-                          "23",
-                          "24",
-                          '25',
-                          "26",
-                          "27",
-                          "28",
-                          '29'
-                        ],
-                        label: age,
-                        selectedItem: age),
-                  ),
+                ),
+                MultiSelectChipGroup(
+                  labelFontSize: 16,
+                  items: [health, diab, obese, hyper, pcos, stress],
+                  onSelectionChanged: (values) {
+                    print(values);
+                  },
+                  horizontalChipSpacing: 10,
+                  selectedColor: Colors.green,
+                  disabledColor: Colors.white,
+                  leftCommonIcon: Icons.perm_identity,
+                  leftIcons: [
+                    Icons.alarm,
+                    Icons.ac_unit,
+                    Icons.accessibility,
+                    Icons.account_balance,
+                    Icons.perm_identity,
+                    Icons.perm_identity,
+                  ],
+                ),
+                  // Align(
+                  //     alignment: Alignment.topLeft,
+                  //     child: Text(
+                  //       infohealth,
+                  //       style: namestyle,
+                  //     )),
+
+                   
+
+                  // Row(children: [
+                  //   Expanded(
+                  //     child: Row(children: <Widget>[
+                  //       Expanded(
+                  //         child: Padding(
+                  //           padding: paddingtb,
+                  //           child: DropdownSearch<String>(
+                  //               mode: Mode.MENU,
+                  //               items: [
+                  //                 "110cm",
+                  //                 "120cm",
+                  //                 "130cm",
+                  //                 '140cm',
+                  //                 "150cm",
+                  //                 "160cm",
+                  //                 "170cm",
+                  //                 '180cm'
+                  //               ],
+                  //               label: height,
+                  //               onChanged: print,
+                  //               selectedItem: selectheight),
+                  //         ),
+                  //       ),
+                  //       SizedBox(width: 20),
+                  //       Expanded(
+                  //         child: Padding(
+                  //           padding: paddingtb,
+                  //           child: DropdownSearch<String>(
+                  //               mode: Mode.MENU,
+                  //               items: [
+                  //                 "30kg",
+                  //                 "31Kg",
+                  //                 "32Kg",
+                  //                 '33Kg',
+                  //                 "34Kg",
+                  //                 "35Kg",
+                  //                 "36Kg",
+                  //                 '37Kg',
+                  //                 "38kg",
+                  //                 "39Kg",
+                  //                 "40Kg",
+                  //                 '41Kg',
+                  //                 "42Kg",
+                  //                 "43Kg",
+                  //                 "44Kg",
+                  //                 '45Kg'
+                  //               ],
+                  //               label: weight,
+                  //               selectedItem: selectweight),
+                  //         ),
+                  //       ),
+                  //     ]),
+                  //   ),
+                  // ]),
+                  // Padding(
+                  //   padding: const EdgeInsets.only(top: 20, bottom: 15),
+                  //   child: DropdownSearch<String>(
+                  //       mode: Mode.MENU,
+                  //       items: [
+                  //         "110cm",
+                  //         "120cm",
+                  //         "130cm",
+                  //         '140cm',
+                  //         "150cm",
+                  //         "160cm",
+                  //         "170cm",
+                  //         '180cm'
+                  //       ],
+                  //       label: height,
+                  //       onChanged: print,
+                  //       selectedItem: selectheight),
+                  // ),
+                  // Padding(
+                  //   padding: paddingtb,
+                  //   child: DropdownSearch<String>(
+                  //       mode: Mode.MENU,
+                  //       items: [
+                  //         "30kg",
+                  //         "31Kg",
+                  //         "32Kg",
+                  //         '33Kg',
+                  //         "34Kg",
+                  //         "35Kg",
+                  //         "36Kg",
+                  //         '37Kg'
+                  //             "38kg",
+                  //         "39Kg",
+                  //         "40Kg",
+                  //         '41Kg',
+                  //         "42Kg",
+                  //         "43Kg",
+                  //         "44Kg",
+                  //         '45Kg'
+                  //       ],
+                  //       label: weight,
+                  //       selectedItem: selectweight),
+                  // ),
+                  // Padding(
+                  //   padding: paddingtb,
+                  //   child: DropdownSearch<String>(
+                  //       mode: Mode.MENU,
+                  //       items: [
+                  //         "14",
+                  //         "15",
+                  //         "16",
+                  //         '17',
+                  //         "18",
+                  //         "19",
+                  //         "20",
+                  //         '21',
+                  //         "22",
+                  //         "23",
+                  //         "24",
+                  //         '25',
+                  //         "26",
+                  //         "27",
+                  //         "28",
+                  //         '29'
+                  //       ],
+                  //       label: age,
+                  //       selectedItem: age),
+                  // ),
+                  // Container(
+                  //   height: 200,
+                  //   child: CupertinoDatePicker(
+                  //     mode: CupertinoDatePickerMode.date,
+                  //     initialDateTime: DateTime(1969, 1, 1),
+                  //     onDateTimeChanged: (DateTime newDateTime) {
+                  //       // Do something
+                  //     },
+                  //   ),
+                  // ),
                   Align(
                       alignment: Alignment.topLeft,
                       child: Text(
@@ -114,7 +208,6 @@ class _RegistrationTwoState extends State<RegistrationTwo> {
                     padding: bottom,
                     child: TextField(),
                   ),
-                  
                   Align(
                       alignment: Alignment.topLeft,
                       child: Text(
@@ -125,7 +218,6 @@ class _RegistrationTwoState extends State<RegistrationTwo> {
                     padding: bottom,
                     child: TextField(),
                   ),
-
                   Padding(
                     padding: const EdgeInsets.only(top: 10, bottom: 5),
                     child: Center(
@@ -140,11 +232,11 @@ class _RegistrationTwoState extends State<RegistrationTwo> {
                 ],
               ),
             ),
-            // ),
-            // ),
           ),
         ),
       ),
     );
   }
 }
+
+showAlertDialog(BuildContext context) {}
--- lib/screens/registration.dart
+++ lib/screens/registration.dart
@@ -38,6 +38,13 @@ class _RegistrationState extends State<Registration> {
                 ),
                 Align(
                     alignment: Alignment.topLeft,
+                    child: Text(email, style: namestyle)),
+                Padding(
+                  padding: bottom,
+                  child: TextField(),
+                ),
+                Align(
+                    alignment: Alignment.topLeft,
                     child: Text(
                       gender,
                       style: namestyle,
@@ -71,34 +78,198 @@ class _RegistrationState extends State<Registration> {
                     ],
                   ),
                 ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 10, bottom: 5, right: 10),
-                  child: Align(
-                      alignment: Alignment.topLeft,
-                      child: Text(
-                        looking,
-                        style: namestyle,
-                      )),
-                ),
-                MultiSelectChipGroup(
-                  labelFontSize: 16,
-                  items: [health, diab, obese, hyper, pcos, stress],
-                  onSelectionChanged: (values) {
-                    print(values);
-                  },
-                  horizontalChipSpacing: 10,
-                  selectedColor: Colors.green,
-                  disabledColor: Colors.white,
-                  leftCommonIcon: Icons.perm_identity,
-                  leftIcons: [
-                    Icons.alarm,
-                    Icons.ac_unit,
-                    Icons.accessibility,
-                    Icons.account_balance,
-                    Icons.perm_identity,
-                    Icons.perm_identity,
+                Row(
+                  children: [
+                    Flexible(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [                         
+                            Text(
+                              '$height (in cm)',
+                              style: TextStyle(
+                                  fontSize: 15, fontWeight: FontWeight.bold),
+                            ),                      
+                          Container(
+                            height: 50,
+                            width: 100,
+                            child: TextField(
+                              decoration: InputDecoration(
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(8.0),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(width: 20),
+                    Flexible(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [                         
+                            Text(
+                              '$weigh (in kg)',
+                              style: TextStyle(
+                                  fontSize: 15, fontWeight: FontWeight.bold),
+                            ),                     
+                          Container(
+                            height: 50,
+                            width: 100,
+                            child: TextField(
+                              decoration: InputDecoration(
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(8.0),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(width: 20),
+                    Flexible(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [                         
+                            Text(
+                              age,
+                              style: TextStyle(
+                                  fontSize: 15, fontWeight: FontWeight.bold),
+                            ),                      
+                          Container(
+                            height: 50,
+                            width: 100,
+                            child: TextField(
+                              decoration: InputDecoration(
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(8.0),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
                   ],
                 ),
+                // Flexible(
+                //   child: Row(
+                //     children: [
+                //       TextField(),
+                //       TextField(),
+                //     ],
+                //   ),
+                // ),
+
+                // Padding(
+                //   padding: const EdgeInsets.only(top: 10, bottom: 5, right: 10),
+                //   child: Align(
+                //       alignment: Alignment.topLeft,
+                //       child: Text(
+                //         looking,
+                //         style: namestyle,
+                //       )),
+                // ),
+                // MultiSelectChipGroup(
+                //   labelFontSize: 16,
+                //   items: [health, diab, obese, hyper, pcos, stress],
+                //   onSelectionChanged: (values) {
+                //     print(values);
+                //   },
+                //   horizontalChipSpacing: 10,
+                //   selectedColor: Colors.green,
+                //   disabledColor: Colors.white,
+                //   leftCommonIcon: Icons.perm_identity,
+                //   leftIcons: [
+                //     Icons.alarm,
+                //     Icons.ac_unit,
+                //     Icons.accessibility,
+                //     Icons.account_balance,
+                //     Icons.perm_identity,
+                //     Icons.perm_identity,
+                //   ],
+                // ),
+                Align(
+                  alignment: Alignment.bottomLeft,
+                  child: Text(
+                    'Blood Group',
+                    style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+                  ),
+                ),
+
+                SizedBox(
+                  height: 50,
+                  child: ListView(
+                    scrollDirection: Axis.horizontal,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: CircleAvatar(
+                          backgroundColor: Colors.grey[100],
+                          child: Text('A+'),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: CircleAvatar(
+                          backgroundColor: Colors.grey[100],
+                          child: Text('A-'),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: CircleAvatar(
+                          backgroundColor: Colors.grey[100],
+                          child: Text('B+'),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: CircleAvatar(
+                          backgroundColor: Colors.grey[100],
+                          child: Text('B-'),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: CircleAvatar(
+                          backgroundColor: Colors.grey[100],
+                          child: Text('AB+'),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: CircleAvatar(
+                          backgroundColor: Colors.grey[100],
+                          child: Text('A'),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: CircleAvatar(
+                          backgroundColor: Colors.grey[100],
+                          child: Text('AB-'),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: CircleAvatar(
+                          backgroundColor: Colors.grey[100],
+                          child: Text('O+'),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: CircleAvatar(
+                          backgroundColor: Colors.grey[100],
+                          child: Text('O-'),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+
                 Padding(
                   padding: const EdgeInsets.only(top: 10, bottom: 5, right: 10),
                   child: Align(
