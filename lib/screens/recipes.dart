952bc318a13b0a4ebd17cce19e92e57542e4513c
Deepanjali Tripathi <deepanjalitripathi7@gmail.com>
2021-12-17 18:30:59 +0530

    recipe screen

--- lib/screens/recipes.dart
+++ lib/screens/recipes.dart
@@ -11,81 +11,83 @@ class Recipe extends StatefulWidget {
 class _RecipeState extends State<Recipe> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Padding(
-          padding: lefttop,
-          child: Align(
-            alignment: Alignment.bottomLeft,
-            child: Text(
-              foodname,
-              style: bold,
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          Padding(
+            padding: lefttop,
+            child: Align(
+              alignment: Alignment.bottomLeft,
+              child: Text(
+                foodname,
+                style: bold,
+              ),
             ),
           ),
-        ),
-        Padding(
-          padding: lefttop,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              Row(
-                children: [
-                  Icon(Icons.lock_clock),
-                  Text(duration),
-                ],
-              ),
-              Row(
-                children: [
-                  Icon(Icons.lock_clock),
-                  Text(duration),
-                ],
-              ),
-              Row(
-                children: [
-                  Icon(Icons.food_bank),
-                  Text(cal),
-                ],
-              ),
-            ],
-          ),
-        ),
-        Padding(
-          padding: lefttop,
-          child: Align(
-            alignment: Alignment.bottomLeft,
-            child: Text(
-              ingredients,
-              style: bold,
+          Padding(
+            padding: lefttop,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Row(
+                  children: [
+                    Icon(Icons.lock_clock),
+                    Text(duration),
+                  ],
+                ),
+                Row(
+                  children: [
+                    Icon(Icons.lock_clock),
+                    Text(duration),
+                  ],
+                ),
+                Row(
+                  children: [
+                    Icon(Icons.food_bank),
+                    Text(cal),
+                  ],
+                ),
+              ],
             ),
           ),
-        ),
-        ListTile(
-          leading: Text(ingredients),
-          trailing: Text(cal),
-        ),
-        ListTile(
-          leading: Text(ingredients),
-          trailing: Text(cal),
-        ),
-        Padding(
-          padding: lefttop,
-          child: Align(
-            alignment: Alignment.bottomLeft,
-            child: Text(
-              steps,
-              style: bold,
+          Padding(
+            padding: lefttop,
+            child: Align(
+              alignment: Alignment.bottomLeft,
+              child: Text(
+                ingredients,
+                style: bold,
+              ),
             ),
           ),
-        ),
-        ListTile(
-          leading: Icon(Icons.circle),
-          title: Text(cal),
-        ),
-        ListTile(
-          leading: Icon(Icons.circle),
-          title: Text(cal),
-        ),
-      ],
+          ListTile(
+            leading: Text(ingredients),
+            trailing: Text(cal),
+          ),
+          ListTile(
+            leading: Text(ingredients),
+            trailing: Text(cal),
+          ),
+          Padding(
+            padding: lefttop,
+            child: Align(
+              alignment: Alignment.bottomLeft,
+              child: Text(
+                steps,
+                style: bold,
+              ),
+            ),
+          ),
+          ListTile(
+            leading: Icon(Icons.circle),
+            title: Text(dummy),
+          ),
+          ListTile(
+            leading: Icon(Icons.circle),
+            title: Text(dummy),
+          ),
+        ],
+      ),
     );
   }
 }
