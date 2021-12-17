038a671a8c5332e91eb396443b4b53769fe62bd3
Deepanjali Tripathi <deepanjalitripathi7@gmail.com>
2021-12-17 18:44:45 +0530

    routing

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,19 @@
 import 'dart:async';
 import 'package:ecojivan/screens/bottomnav.dart';
 import 'package:ecojivan/screens/consult.dart';
+import 'package:ecojivan/screens/cuisines.dart';
+import 'package:ecojivan/screens/date.dart';
 import 'package:ecojivan/screens/diet.dart';
 import 'package:ecojivan/screens/doctors.dart';
 import 'package:ecojivan/screens/edit.dart';
+import 'package:ecojivan/screens/healthyrecipe.dart';
+import 'package:ecojivan/screens/indian.dart';
 import 'package:ecojivan/screens/payment.dart';
+import 'package:ecojivan/screens/physiolist.dart';
 import 'package:ecojivan/screens/product.dart';
+import 'package:ecojivan/screens/productdesc.dart';
+import 'package:ecojivan/screens/psych.dart';
+import 'package:ecojivan/screens/recipes.dart';
 import 'package:ecojivan/screens/registertwo.dart';
 import 'package:ecojivan/screens/registration.dart';
 import 'package:ecojivan/screens/reports.dart';
@@ -14,6 +22,8 @@ import 'package:ecojivan/screens/sliverappbar.dart';
 import 'package:ecojivan/screens/splash.dart';
 import 'package:ecojivan/screens/verification.dart';
 import 'package:ecojivan/screens/workout.dart';
+import 'package:ecojivan/screens/workoutdesc.dart';
+import 'package:ecojivan/screens/workoutlist.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -46,6 +56,14 @@ class MyApp extends StatelessWidget {
         '/product': (context) => Product(),
         '/reports': (context) => WeeklyReports(),
         '/workout': (context) => Workout(),
+        '/workoutlist': (context) => WorkoutList(),
+        '/workoutdesc': (context) => WorkoutDescription(),
+        '/cuisine': (context) => Cuisines(),
+        '/indian': (context) => Indian(),
+        '/healthy': (context) => HealthyRecipe(),
+        '/productdesc': (context) => ProductDescription(),
+        '/physio': (context) => PhysioList(),
+        '/psych': (context) => PsychTherapy()
       },
     );
   }
