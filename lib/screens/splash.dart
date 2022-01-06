8ad23c492668590274d83920462729426be9b075
Deepanjali Tripathi <deepanjalitripathi7@gmail.com>
2022-01-06 18:09:58 +0530

    conditional navigation

--- lib/screens/splash.dart
+++ lib/screens/splash.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:ecojivan/constraint.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class SplashScreen extends StatefulWidget {
@@ -11,11 +12,27 @@ class _SplashScreenState extends State<SplashScreen> {
 @override
 void initState() {
 	super.initState();
-	Timer(Duration(seconds: 3),
-		()=>
-    Navigator.of(context).pushNamedAndRemoveUntil('/verification', (route) => false),
-		);
+   startTimer();
 }
+
+
+ void navigateUser() async{
+    if (FirebaseAuth.instance.currentUser != null) {
+      Navigator.of(context).pushNamedAndRemoveUntil('/home', (route) => false);
+    } else {
+     Navigator.of(context).pushNamedAndRemoveUntil('/verification', (route) => false);
+    }
+  }
+
+
+void startTimer() {
+    Timer(Duration(seconds: 3), () {
+      navigateUser();
+    });
+  }
+
+
+
 @override
 Widget build(BuildContext context) {
 	return Container(
